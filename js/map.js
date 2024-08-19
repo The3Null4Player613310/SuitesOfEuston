a383256cb5eb982ff2f6394ce8a965c942d46ac3
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2024-08-19 04:17:53 -0300

    Update map.js
    Added map.js.

---
+++ js/map.js
@@ -0,0 +1,26 @@
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+  key: "YOUR_API_KEY",
+  v: "weekly"
+});
+
+let map;
+
+async function initMap() {
+  const position = { lat: 0, lng: 0}
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("MAP"), {
+    zoom: 18,
+    center: position,
+    mapId: "MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Suites of Euston",
+  });
+}
+
+initMap();
