818a1c60e0b9803cafe9ecc3c5e0d99431f9ed5b
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2024-08-20 01:21:00 -0300

    Update bike.js
    Updated bike.js.

--- js/bike.js
+++ js/bike.js
@@ -7,7 +7,7 @@ function bikeSubmit()
   var msg = document.getElementById("H-MSG").value;
   var dateStart = document.getElementById("DATE-START").value;
   var dateEnd = document.getElementById("DATE-END").value;
-  var body = msg + "%0A" + dateStart + " - " + dateEnd;
+  var body = msg + " " + dateStart + " - " + dateEnd;
   document.getElementById("H-BODY").value = body;
   return true;
 }
