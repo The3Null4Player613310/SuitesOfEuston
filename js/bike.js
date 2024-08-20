65de9aee27a753ed8679dca9db0d012b675ffeb6
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2024-08-20 05:48:56 +0000

    Update bike.js Updated bike.js.

--- js/bike.js
+++ js/bike.js
@@ -7,7 +7,7 @@ function bikeSubmit()
   var msg = document.getElementById("H-MSG").value;
   var dateStart = document.getElementById("DATE-START").value;
   var dateEnd = document.getElementById("DATE-END").value;
-  var body = ("").concat(msg, "&nbsp;FROM&nbsp;", dateStart, "&nbsp;TO:&nbsp;", dateEnd);
+  var body = ("").concat(msg, "&nbsp;FROM:&nbsp;", dateStart, "&nbsp;TO:&nbsp;", dateEnd);
   alert(body);
   document.getElementById("H-BODY").value = body;
   return true;
