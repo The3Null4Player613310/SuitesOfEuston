8ef51a4791a39e58570736ddc0120e4767d11573
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2024-08-20 15:26:57 -0300

    Update room.js
    Updated room.js.

--- js/room.js
+++ js/room.js
@@ -10,18 +10,9 @@ function roomSubmit(event)
 {
   
   var valR = true;
-  //var ws = document.getElementById("H-WS").value;
-  //var msg = document.getElementById("H-MSG").value;
+
   var dateStart = document.getElementById("FORM-ROOM-DATE-CHECK-IN").value;
   var dateEnd = document.getElementById("FORM-ROOM-DATE-CHECK-OUT").value;
-  //var body = ("").concat(msg, ws, "\n", "FROM:", ws, dateStart, ws, "TO:", ws, dateEnd);
-  //alert(body);
-  //alert(navigator.platform);
-  
-  //if(navigator.platform === "iPhone") valR = true;
-  //if(navigator.platform === "MacIntel") valR = false;
-  
-  //document.getElementById("FORM-ROOM-H-BODY").value = msg;
   
   if(dateStart > dateEnd)
   {
@@ -31,7 +22,7 @@ function roomSubmit(event)
   
   if(valR)
   {
-    //document.getElementById("FORM-ROOM-H-BODY").value = body;
+    
   }
   else
   {
