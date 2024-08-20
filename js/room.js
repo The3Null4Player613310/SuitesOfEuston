43246c546cd98531ad8ddf526aa426a9c143f478
Allison Munn <the.3null.4player.6.13.3.10.@gmail.com>
2024-08-20 15:17:44 -0300

    Update room.js
    Updated room.js.

--- js/room.js
+++ js/room.js
@@ -10,18 +10,18 @@ function roomSubmit(event)
 {
   
   var valR = true;
-  var ws = document.getElementById("H-WS").value;
-  var msg = document.getElementById("H-MSG").value;
-  var dateStart = document.getElementById("DATE-START").value;
-  var dateEnd = document.getElementById("DATE-END").value;
-  var body = ("").concat(msg, ws, "\n", "FROM:", ws, dateStart, ws, "TO:", ws, dateEnd);
+  //var ws = document.getElementById("H-WS").value;
+  //var msg = document.getElementById("H-MSG").value;
+  var dateStart = document.getElementById("FORM-ROOM-DATE-CHECK-IN").value;
+  var dateEnd = document.getElementById("FORM-ROOM-DATE-CHECK-OUT").value;
+  //var body = ("").concat(msg, ws, "\n", "FROM:", ws, dateStart, ws, "TO:", ws, dateEnd);
   //alert(body);
   //alert(navigator.platform);
   
   //if(navigator.platform === "iPhone") valR = true;
   //if(navigator.platform === "MacIntel") valR = false;
   
-  document.getElementById("H-BODY").value = msg;
+  //document.getElementById("FORM-ROOM-H-BODY").value = msg;
   
   if(dateStart > dateEnd)
   {
@@ -31,7 +31,7 @@ function roomSubmit(event)
   
   if(valR)
   {
-    document.getElementById("H-BODY").value = body;
+    //document.getElementById("FORM-ROOM-H-BODY").value = body;
   }
   else
   {
